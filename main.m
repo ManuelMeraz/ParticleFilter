0b642065155e57356f9824314473dc712f008c02
Manuel Meraz <meraz.manuel@gmail.com>
2017-11-04 12:35:59 -0700

    motion model implemented

--- main.m
+++ main.m
@@ -20,3 +20,28 @@ for i = 1:size(C)(1,1)
     x(i) = rand * ( C(i, 2) - C(i, 1) ) + C(i, 1);
 end
 
+% Print out inital state
+x = x
+
+data(1,1) = x(1);
+data(1,2) = x(2);
+
+% For the motion model
+dt = 1;
+
+for i = 1:length(inputs.commands)
+
+    u = inputs.commands(i,:).';
+    z = inputs.sensor_readings(i,:).';
+
+    x = sample_motion_model_velocity(u, x, dt)
+
+    data(i,1) = x(1);
+    data(i,2) = x(2);
+
+    % Plot Settings
+    scatter(data(1:i,1), data(1:i,2), 5, 'b');
+    drawnow;
+    pause(0.1);
+
+end
--- sample_motion_model_velocity.m
+++ sample_motion_model_velocity.m
@@ -1,3 +1,20 @@
-function xt = sample_motion_model_velocity(ut, xt)
-    z   
+function x = sample_motion_model_velocity(u, x, dt)
+    % Alpha 1-4
+    a_1_4 = 0.01;
+
+    % Alpha 4-5
+    a_4_5 = 0.0005;    
+
+    v = u(1);
+    omega = u(2);
+
+    v = v + rand * (a_1_4 * v + a_1_4 * omega);
+    omega = omega + rand * (a_1_4 * v + a_1_4 * omega);
+    gamma =  rand * (a_4_5 * v + a_4_5 * omega);
+
+    weight = v / omega;
+
+    x(1) = x(1) + weight * ( -sin(x(3)) + sin(x(3) + omega * dt) );
+    x(2) = x(2) + weight * ( cos(x(3)) - cos(x(3) + omega * dt) );
+    x(3) = x(3) + dt * (gamma + omega);
 end
